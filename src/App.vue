56d5fbe9a2ef554fc024f15d74a86119bbdf74fd
Manuel Greulich <manuelgreulich@ahoo.com>
2023-09-03 17:38:46 +0200

    added slots & props

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,24 @@
     <div id="app"></div>
     Hi, {{ name_first }}!
 
-    <button @click="test">test</button>
+    <navigationLink url="https://www.google.de">
+        <template v-slot:image>
+            <img
+                src="https://1000marken.net/wp-content/uploads/2021/01/Google-logo.png"
+            />
+        </template>
+
+        Zum Profil</navigationLink
+    >
+
+    <!-- <a href="?seite=23" @click.prevent="test">Gehe zu Seite 23</a> -->
+    <!-- <button @click="test">test</button> -->
     <todoList />
 </template>
 
 <script>
 import todoList from "./components/todoList.vue";
+import navigationLink from "./components/navigationLink.vue";
 
 export default {
     name: "App",
@@ -16,14 +28,15 @@ export default {
         name_first: "Manuel",
     }),
 
+    components: {
+        todoList,
+        navigationLink,
+    },
+
     methods: {
         test() {
             alert("hi");
         },
-    },
-
-    components: {
-        todoList,
     },
 };
 </script>
@@ -35,6 +48,10 @@ body {
 
 .finishedTasks {
     text-decoration: line-through;
+}
+
+img {
+    height: 20px;
 }
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/navigationLink.vue
@@ -0,0 +1,24 @@
+<template>
+    <a :href="url" class="nav-link">
+        <slot name="image"></slot>|
+
+        <slot></slot>
+    </a>
+</template>
+
+<script>
+export default {
+    props: ["url"],
+
+    name: "navigationLink",
+};
+</script>
+
+<style>
+a:link,
+a:active,
+a:visited,
+a:hover {
+    color: #f00;
+}
+</style>
